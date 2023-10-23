97ce9c84dde644c767d5d944e0ed4f64aa4a2ad5
dodamartin <146016722+dodamartin@users.noreply.github.com>
2023-10-23 19:01:47 +0200

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,25 @@
+import './App.css';
+import RegistrationForm from './component/RegistrationForm';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import { Dashboard } from './component/Dashboard';
+import PasswordResetForm from './component/PasswordResetForm';
+import LoginForm from './component/LoginForm';
+import Auth from './component/Auth';
 
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <Routes>
+            <Route path = '/' element = {<Auth />}/>
+            <Route path = '/dashboard' element = {<Dashboard/>}/>
+            <Route path = '/reset' element = {<PasswordResetForm />}/>
+            <Route path = '/login' element = {<LoginForm />}/>
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
